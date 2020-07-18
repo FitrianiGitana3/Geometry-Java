08ceec99d7badf34b57f02e677c1f4adae26ff48
Samuel <67509713+samnunel56@users.noreply.github.com>
2020-07-18 22:10:42 +0700

    Update

--- src/main/java/com/esri/core/geometry/AtributStreamBase.java
+++ src/main/java/com/esri/core/geometry/AtributStreamBase.java
@@ -1,4 +1,7 @@
-    
+    public static AttributeStreamBase createByteStream(int size) {
+        AttributeStreamBase newStream = new AttributeStreamOfInt8(size);
+        return newStream;
+    }
 
 
 public static AttributeStreamBase createByteStream(int size) {
