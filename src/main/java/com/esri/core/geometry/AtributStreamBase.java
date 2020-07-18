7334e1cf54b0f0fcf26c58df8993cac841633687
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-07-18 22:02:10 +0700

    Update

--- src/main/java/com/esri/core/geometry/AtributStreamBase.java
+++ src/main/java/com/esri/core/geometry/AtributStreamBase.java
@@ -5,3 +5,14 @@ public static AttributeStreamBase createByteStream(int size) {
         AttributeStreamBase newStream = new AttributeStreamOfInt8(size);
         return newStream;
     }
+
+
+
+    public static AttributeStreamBase createByteStream(int size,
+            byte defaultValue) {
+        AttributeStreamBase newStream = new AttributeStreamOfInt8(size,
+                defaultValue);
+        return newStream;
+
+    }
+
